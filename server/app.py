8b44e171fce637e1a2da2d5f8cbe8d649ee2b992
NICKMUGO <nicknjuru13@gmail.com>
2024-03-05 09:46:44 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -17,11 +17,44 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get():
+        plants=Plant.query.all()
+        db.session.add_all(plants)
+        db.session.commit()
+        plants_dict=[plant.to_dict() for plant in plants ]
 
-class PlantByID(Resource):
-    pass
+
+        return make_response(jsonify(plants_dict), 200)
+    
+    def post():
+        name=request.form.get("name")
+        price=request.form.get("price")
+        image=request.form.get("image")
+
+        new_plant=Plant(
+            name=name,
+            price=float(price),
+            image=image
+        )
+        try:
+            db.session.add(new_plant)
+            db.session.commit()
+            return jsonify({"message": "New plant added successfully."}), 201
+        except Exception as e:
+            print(e)
+            return jsonify({"error":"There was an error adding the plant"}),500
         
-
+api.add_resource(Plants, '/plants')
+class PlantByID(Resource):
+    def get(id):
+        plant=Plant.query.filter_by(id=id).first()
+        db.session.add_all(plant)
+        db.session.commit()
+        plant_dict=plant.to_dict()
+    
+       
+        return make_response(jsonify(plant_dict), 200)
+        
+api.add_resource(PlantByID, '/plants/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
---
+++ server/migrations/versions/ac67f68bb7b5_finished_plant_table.py
@@ -0,0 +1,24 @@
+"""Finished plant table
+
+Revision ID: ac67f68bb7b5
+Revises: 67f5d67aea55
+Create Date: 2024-03-03 19:45:34.809353
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ac67f68bb7b5'
+down_revision = '67f5d67aea55'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
